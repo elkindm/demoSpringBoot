e6029a1ea297066042cb199e9ccc1767e2bfff11
elkindm <elkinduranm@gmail.com>
2022-04-06 15:35:07 -0500

    se realiza metodo inicio y se agrega librerias de logger de lombok y se configura el properties para configurar el puerto, nivel del logger formato de fecha en el logger

--- demoSpringBoot/src/main/java/co/com/libosoft/demoSpringBoot/controller/ControladorInicio.java
+++ demoSpringBoot/src/main/java/co/com/libosoft/demoSpringBoot/controller/ControladorInicio.java
@@ -3,6 +3,12 @@ package co.com.libosoft.demoSpringBoot.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+
+
+
 /**
  * 
  * @author duran
@@ -10,10 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@Slf4j
 public class ControladorInicio {
-
+	
+	private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ControladorInicio.class);
 	@GetMapping("/")
 	public String inicio() {
-		return "Hola mundo con Spring";
+		
+		log.info("iniciando controlador inicio");
+		log.debug("datos debug");
+		return "Hola mundo con Spring 2";
 	}
 }

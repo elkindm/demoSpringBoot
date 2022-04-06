da5ad395b394d41ddbbd4cffca330f8465e7f643
elkindm <elkinduranm@gmail.com>
2022-04-06 00:03:23 -0500

    commit inicial del proyecto curso spring boot clase 1 2022-04-05

---
+++ demoSpringBoot/src/main/java/co/com/libosoft/demoSpringBoot/DemoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package co.com.libosoft.demoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootApplication.class, args);
+	}
+
+}
